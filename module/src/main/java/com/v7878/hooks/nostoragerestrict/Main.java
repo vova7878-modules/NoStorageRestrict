92d7eb45f2c126d3e149eed947e3cc5c000e89d7
v7878 <46866705+vova7878@users.noreply.github.com>
2024-11-10 17:56:54 +0700

    Add missing annotations

--- module/src/main/java/com/v7878/hooks/nostoragerestrict/Main.java
+++ module/src/main/java/com/v7878/hooks/nostoragerestrict/Main.java
@@ -3,11 +3,19 @@ package com.v7878.hooks.nostoragerestrict;
 import android.util.Log;
 
 import com.github.kr328.zloader.ZygoteLoader;
+import com.v7878.r8.annotations.DoNotObfuscate;
+import com.v7878.r8.annotations.DoNotObfuscateType;
+import com.v7878.r8.annotations.DoNotShrink;
+import com.v7878.r8.annotations.DoNotShrinkType;
 
+@DoNotShrinkType
+@DoNotObfuscateType
 public class Main {
     public static String TAG = "NO_STORAGE_RESTRICT";
 
     @SuppressWarnings({"unused", "ConfusingMainMethod"})
+    @DoNotShrink
+    @DoNotObfuscate
     public static void main() throws Throwable {
         Log.w(TAG, "Injected into " + ZygoteLoader.getPackageName());
         LoadedApkHook.init();
